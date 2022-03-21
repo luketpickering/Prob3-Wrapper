d15baea86b78293684e0883cc0173a7ac8f7ade3
Luke Pickering <luketpickering@googlemail.com>
2022-03-21 09:22:50 +0000

    Update build script to use the github version of prob3++

--- build.sh
+++ build.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-if [ -z "${ROOTSYS}" ]; then
-  echo "[ERROR]: \$ROOTSYS is not defined, did you set up your ROOT environment."
+if ! hash root-config; then
+  echo "[ERROR]: Cannot find root-config, is ROOT set up?"
   exit 1
 fi
 
-PROB3PPVERSION="20121225"
-
-if [ ! -e Prob3++.${PROB3PPVERSION}.tar.gz ] && [ ! -e Prob3++ ]; then
-    wget http://webhome.phy.duke.edu/~raw22/public/Prob3++/Prob3++.${PROB3PPVERSION}.tar.gz
-    tar -zxvf Prob3++.${PROB3PPVERSION}.tar.gz
-    mv Prob3++.${PROB3PPVERSION} Prob3++
+if [ ! -e Prob3plusplus ]; then
+    git clone https://github.com/rogerwendell/Prob3plusplus.git
 fi
 
-if [ ! -e Prob3++/libThreeProb_2.10.a ]; then
-  cd Prob3++
+if [ ! -e Prob3plusplus/libThreeProb*.a ]; then
+  cd Prob3plusplus
   CXXFLAGS=-fPIC CFLAGS=-fPIC make
   cd ../
 fi
 
-g++ OscillationHelper.cxx -I Prob3++ Prob3++/libThreeProb_2.10.a -fPIC -shared -o WrappedProb3++.${PROB3PPVERSION}.so $(root-config --cflags --libs)
+PROB3LIBNAME=$(ls Prob3plusplus/libThreeProb*.a)
 
+CMD="g++ OscillationHelper.cxx -I Prob3plusplus ${PROB3LIBNAME} -fPIC -shared -o WrappedProb3plusplus.so $(root-config --cflags --libs)"
+
+echo $CMD
+${CMD}
 
 echo "#pragma once" > Prob3ppWrapper.hxx
 echo "#include \"$(pwd)/OscillationHelper.hxx\"" >> Prob3ppWrapper.hxx
-echo "#pragma cling load(\"$(pwd)/WrappedProb3++.${PROB3PPVERSION}.so\")" >> Prob3ppWrapper.hxx
+echo "#pragma cling load(\"$(pwd)/WrappedProb3plusplus.so\")" >> Prob3ppWrapper.hxx
+
+echo "cat Prob3ppWrapper.hxx"
+cat Prob3ppWrapper.hxx
